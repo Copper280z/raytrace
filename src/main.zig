a4a8a65b79db9769e74648c13536872826bb4345
copper280z <rday689@gmail.com>
2024-11-02 15:23:01 -0400

    implement threading for rendering

--- src/main.zig
+++ src/main.zig
@@ -275,10 +275,10 @@ pub fn build_BVH(parent_arr: *std.ArrayList(Sphere), obj_inds: []usize, depth: u
         objects.append(.{ .ind = ind, .obj = parent_arr.*.items[ind] }) catch unreachable;
     }
 
-    var min_corner: vec_type = undefined;
-    var max_corner: vec_type = undefined;
+    var min_corner: vec_type = .{ 99999.0, 99999.0, 99999.0 };
+    var max_corner: vec_type = .{ -99999.0, -99999.0, -99999.0 };
     for (objects.items) |obj| {
-        for (0..2) |i| {
+        for (0..3) |i| {
             if ((obj.obj.center[i] - obj.obj.radius) < min_corner[i]) {
                 min_corner[i] = obj.obj.center[i] - obj.obj.radius;
             }
@@ -289,14 +289,7 @@ pub fn build_BVH(parent_arr: *std.ArrayList(Sphere), obj_inds: []usize, depth: u
     }
     // std.debug.print("BBOX for this group: min {d:.3},{d:.3},{d:.3} , max {d:.3},{d:.3},{d:.3}\n", .{ min_corner[0], min_corner[1], min_corner[2], max_corner[0], max_corner[1], max_corner[2] });
     const new_bbox = AxisAlignedBBox.make(min_corner, max_corner);
-    std.debug.print("interval x: {d:.3},{d:.3}, y: {d:.3},{d:.3}, z: {d:.3},{d:.3}\n", .{
-        new_bbox.x.min,
-        new_bbox.x.max,
-        new_bbox.y.min,
-        new_bbox.y.max,
-        new_bbox.z.min,
-        new_bbox.z.max,
-    });
+
     if ((depth == 0) or (obj_inds.len <= 3)) {
         // var leaf = allocator.create(LeafNode) catch unreachable;
 
@@ -312,9 +305,17 @@ pub fn build_BVH(parent_arr: *std.ArrayList(Sphere), obj_inds: []usize, depth: u
         node.* = .{ .leaf = leaf };
 
         // var out = TreeNode{ .leaf = node[0] };
-        std.debug.print("Made a leaf with {} items\n", .{node.leaf.inds.len});
+        // std.debug.print("Made a leaf with {} items\n", .{node.leaf.inds.len});
         return node;
     }
+    std.debug.print("interval x: {d:.3},{d:.3}, y: {d:.3},{d:.3}, z: {d:.3},{d:.3}\n", .{
+        new_bbox.x.min,
+        new_bbox.x.max,
+        new_bbox.y.min,
+        new_bbox.y.max,
+        new_bbox.z.min,
+        new_bbox.z.max,
+    });
     // split the list of objects in half along the longest axis
     const dimensions = max_corner - min_corner;
     const biggest_ax = vec3f.argmax(dimensions);
@@ -325,6 +326,7 @@ pub fn build_BVH(parent_arr: *std.ArrayList(Sphere), obj_inds: []usize, depth: u
         sorted_inds.append(obj.ind) catch unreachable;
     }
     const thresh = dimensions[biggest_ax] / 2 + min_corner[biggest_ax];
+    std.debug.print("Threshold position: {d:.3} on ax: {}\n", .{ thresh, biggest_ax });
     var thresh_idx: u32 = 1;
     for (objects.items) |obj| {
         const point = obj.obj.center[biggest_ax] + obj.obj.radius;
@@ -333,6 +335,17 @@ pub fn build_BVH(parent_arr: *std.ArrayList(Sphere), obj_inds: []usize, depth: u
         }
         thresh_idx += 1;
     }
+    var num_below: u32 = 0;
+    var num_above: u32 = 0;
+    for (objects.items) |obj| {
+        const point = obj.obj.center[biggest_ax] + obj.obj.radius;
+        if (point > thresh) {
+            num_above += 1;
+        } else {
+            num_below += 1;
+        }
+    }
+    std.debug.print("{} above thresh, {} below thresh\n", .{ num_above, num_below });
     thresh_idx = @min(thresh_idx, obj_inds.len - 1);
     // var node = allocator.alloc(BranchNode, 1) catch unreachable;
     // errdefer allocator.free(node);
@@ -343,7 +356,7 @@ pub fn build_BVH(parent_arr: *std.ArrayList(Sphere), obj_inds: []usize, depth: u
     } else {
         split = @intCast(obj_inds.len / 2);
     }
-    std.debug.print("Adding branch node at depth {}, with {} objects remaining, splitting at {}, along {}\n", .{ depth, obj_inds.len, thresh_idx, biggest_ax });
+    std.debug.print("Adding branch node at depth {}, with {} objects remaining, splitting at {}, along {}\n\n", .{ depth, obj_inds.len, thresh_idx, biggest_ax });
     node.bbox = new_bbox;
     node.child1 = build_BVH(parent_arr, sorted_inds.items[0..split], depth - 1, allocator);
     node.child2 = build_BVH(parent_arr, sorted_inds.items[split..], depth - 1, allocator);
@@ -491,21 +504,21 @@ pub fn check_neg3(v: vec_type) void {
     //     @panic("Pixel value out of range! Negative!");
     // }
 }
-pub fn Render(world: Hittable, shape: shape_s, allocator: Allocator, rng: anytype) !std.ArrayList(@Vector(3, u8)) {
+pub fn Render(world: Hittable, shape: shape_s, seed: u64, img: *std.ArrayList(@Vector(3, u8))) !void {
     @setFloatMode(.optimized);
-    var img = try std.ArrayList(@Vector(3, u8)).initCapacity(allocator, shape.nx * shape.ny);
+    var rnd = RndGen.init(seed);
     log.info("Starting ray trace", .{});
     for (0..shape.ny) |jr| {
         for (0..shape.nx) |i| {
             const j = shape.ny - jr;
-            const pix = trace(i, j, shape, rng, world);
+            const pix = trace(i, j, shape, rnd.random(), world);
             check_nan3(pix);
             // std.debug.print("pix: {d:.3},{d:.3},{d:.3}\n", .{ pix[0], pix[1], pix[2] });
             const ipix: @Vector(3, u8) = @intFromFloat(@as(vec_type, @splat(255.99)) * pix);
-            img.appendAssumeCapacity(ipix);
+            try img.append(ipix);
         }
     }
-    return img;
+    // return img;
 }
 
 pub fn trace(i: usize, j: usize, shape: shape_s, rng: anytype, world: Hittable) vec_type {
@@ -555,16 +568,49 @@ pub fn random_in_unit_sphere(rng: anytype) vec_type {
 }
 
 pub fn render_and_write(fname: []const u8, world: Hittable, shape: shape_s, filetype: img_io.ImgType, allocator: Allocator) !void {
-    var rnd = RndGen.init(456);
     const t0 = std.time.milliTimestamp();
-    const img = try Render(world, shape, allocator, rnd.random());
-    defer img.deinit();
+    var final_img = try std.ArrayList(@Vector(3, u8)).initCapacity(allocator, shape.nx * shape.ny);
+    defer final_img.deinit();
+
+    if (std.Thread.getCpuCount()) |num_cpus| {
+        const img_type = std.ArrayList(@Vector(3, u8));
+        var thread_config = std.Thread.SpawnConfig{};
+        thread_config.allocator = allocator;
+        var threads = std.ArrayList(std.Thread).init(allocator);
+        var images = std.ArrayList(img_type).init(allocator);
+        for (0..num_cpus) |i| {
+            const img = try img_type.initCapacity(allocator, shape.nx * shape.ny);
+
+            try images.append(img);
+            const handle =
+                try std.Thread.spawn(thread_config, Render, .{ world, shape, i * 123, &images.items[i] });
+            try threads.append(handle);
+        }
+        std.debug.print("Started {} threads\n", .{num_cpus});
+
+        for (threads.items) |thread| {
+            thread.join();
+        }
+        std.debug.print("all threads finished\n", .{});
+
+        for (0..shape.nx * shape.ny) |idx| {
+            var tmp_pix: @Vector(3, u32) = .{ 0, 0, 0 };
+            for (images.items) |img| {
+                tmp_pix += @intCast(img.items[idx]);
+            }
+            // std.debug.print("pix: {d:.3},{d:.3},{d:.3}\n", .{ tmp_pix[0] / num_cpus, tmp_pix[1] / num_cpus, tmp_pix[2] / num_cpus });
+            try final_img.append(@intCast(tmp_pix / @as(@Vector(3, u32), @splat(@intCast(num_cpus)))));
+            // try final_img.append(images.items[0].items[idx]);
+        }
+    } else |_| {
+        try Render(world, shape, 123, &final_img);
+    }
     const t1 = std.time.milliTimestamp();
 
     std.debug.print("Took {d:.3} sec to render\n", .{@as(f32, @floatFromInt(t1 - t0)) / 1000});
     switch (filetype) {
         .ppm => {
-            try img_io.write_ppm_image(fname, .{ .arr = img, .nx = shape.nx, .ny = shape.ny });
+            try img_io.write_ppm_image(fname, .{ .arr = final_img, .nx = shape.nx, .ny = shape.ny });
         },
         else => {
             // return error.Unsupported_image_type;
@@ -592,9 +638,9 @@ pub fn Random_Scene(half_size: usize, rng: std.Random, allocator: std.mem.Alloca
             const choose_mat: u8 = @intFromFloat(rng.float(f32) * 100);
             const center: vec_type = .{ a + 0.9 * rng.float(f32), 0.2, b + 0.9 * rng.float(f32) };
 
-            if (vec3f.length(center - vec_type{ 4.0, 0.2, 0.0 }) > 0.9) {
+            if (vec3f.length(center - vec_type{ 4.0, 0.2, 0.0 }) > 0.9 and (rng.float(f32) > 0.4)) {
                 const sphere: Sphere = switch (choose_mat) {
-                    0...70 => blk: { // Lambertian
+                    0...50 => blk: { // Lambertian
                         const rnd_vec = random_in_unit_sphere(rng);
                         const normed_vec = vec3f.normalize(rnd_vec * rnd_vec);
                         check_neg3(normed_vec);
@@ -602,7 +648,7 @@ pub fn Random_Scene(half_size: usize, rng: std.Random, allocator: std.mem.Alloca
                         check_neg3(albedo);
                         break :blk .{ .center = center, .radius = 0.2, .material = .{ .lambertian = .{ .albedo = albedo } } };
                     },
-                    71...90 => blk: { // Metal
+                    51...80 => blk: { // Metal
                         const rnd_vec = random_in_unit_sphere(rng);
                         const normed_vec = vec3f.normalize(rnd_vec * rnd_vec);
                         check_neg3(normed_vec);
@@ -610,7 +656,7 @@ pub fn Random_Scene(half_size: usize, rng: std.Random, allocator: std.mem.Alloca
                         check_neg3(albedo);
                         break :blk .{ .center = center, .radius = 0.2, .material = .{ .metal = .{ .albedo = albedo, .roughness = 0.3 * rng.float(f32) } } };
                     },
-                    91...95 => // Glass
+                    81...90 => // Glass
                     .{ .center = center, .radius = 0.2, .material = .{ .dielectric = .{ .color = vec3f.Splat(0.95), .index = 1.5 + 8.5 * rng.float(f32) } } },
                     else => blk: { // Glass
                         const rnd_index = rng.float(f32);
@@ -649,7 +695,7 @@ pub fn Random_Scene(half_size: usize, rng: std.Random, allocator: std.mem.Alloca
 var cam = cam_types.MovableCamera{};
 const Hittable = HittableBVH([]const Sphere);
 // const Hittable = HittableList([]const Sphere);
-const num_samples = 100;
+const num_samples = 256 / 8;
 
 pub fn main() !void {
     // comptime {
@@ -658,14 +704,14 @@ pub fn main() !void {
     const filetype = .ppm;
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    const nx = 1920;
-    const ny = 1080;
+    const nx = 1024;
+    const ny = 512;
     var arena = std.heap.ArenaAllocator.init(allocator);
-    cam.SetPosition(.{ 13.0, 2.0, 3.0 }, .{ 0.0, 0.0, 0.0 }, .{ 0.0, 1.0, 0.0 }, 20, @as(f32, @floatFromInt(nx)) / @as(f32, @floatFromInt(ny)));
+    cam.SetPosition(.{ 13.0, 2.0, 3.5 }, .{ 0.0, 0.0, 0.0 }, .{ 0.0, 1.0, 0.0 }, 20, @as(f32, @floatFromInt(nx)) / @as(f32, @floatFromInt(ny)));
     var rng = RndGen.init(0);
 
     // const World = Hittable{ .objects = list[0..] };
     const World = try Random_Scene(11, rng.random(), arena.allocator());
     log.info("Creating {} x {} image", .{ nx, ny });
-    try render_and_write("Scene.ppm", World, .{ .nx = nx, .ny = ny }, filetype, allocator);
+    try render_and_write("Scene.ppm", World, .{ .nx = nx, .ny = ny }, filetype, arena.allocator());
 }
